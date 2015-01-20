41a4d27e8f8ac62d861018a477acbc24d0574c7a
olivierrr <oliviersinbox@gmail.com>
2015-01-20 00:32:50 -0800

    getting game started.

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 
 var Renderer = require('./lib/renderer')
+var Game     = require('./lib/game')
 var fit      = require('canvas-fit')
 var raf      = require('raf')
 
 var canvas = document.body.appendChild(document.createElement('canvas'))
+
 var render = Renderer(canvas)
+var game = Game()
 
 window.addEventListener('resize'
   , fit(canvas)
   , false
 )
 
-//test
-var COLOR = {
-    red: [1.0, 0.0, 0.0]
-  , blue: [0.0, 0.0, 1.0]
-  , green: [0.0, 1.0, 0.0]
-  , black: [1.0, 1.0, 1.0]
-}
-
-var state = [
-  [COLOR.red, COLOR.blue, 0, 0, 0],
-  [COLOR.blue, COLOR.green, 0, 0, 0],
-  [COLOR.green, COLOR.green, 0, 0, 0],
-  [COLOR.green, COLOR.green, 0, 0, COLOR.blue]
-]
+window.setInterval(function() {
+  game.tick()
+}, 1000)
 
 raf(function tick() {
-  render(state)
+  render(game.board())
   raf(tick)
 })
