694436b8617c8716d96159ffd1abf3f04c3d67be
olivierrr <oliviersinbox@gmail.com>
2015-01-19 16:23:32 -0800

    .

--- index.js
+++ index.js
@@ -1,71 +1,31 @@
-var Geometry = require('gl-geometry')
+
+var Renderer = require('./lib/renderer')
 var fit      = require('canvas-fit')
-var mat4     = require('gl-mat4')
-var normals  = require('normals')
-var glslify  = require('glslify')
-var cube     = require('a-cube')
+var raf      = require('raf')
 
 var canvas = document.body.appendChild(document.createElement('canvas'))
-var camera = require('canvas-orbit-camera')(canvas)
-var gl = require('gl-context')(canvas, render)
+var render = Renderer(canvas)
 
 window.addEventListener('resize'
   , fit(canvas)
   , false
 )
 
-var geometry = Geometry(gl)
-
-geometry.attr('aPosition', cube.positions)
-geometry.faces(cube.cells)
-
-var projection = mat4.create()
-var model      = mat4.create()
-var view       = mat4.create()
-var height
-var width
-
-var shader = glslify({
-    vert: './shaders/main.vert'
-  , frag: './shaders/main.frag'
-})(gl)
-
-function update() {
-
-  width  = gl.drawingBufferWidth
-  height = gl.drawingBufferHeight
-
-  camera.view(view)
-  camera.tick()
-
-  // Update projection matrix.
-  var aspectRatio = gl.drawingBufferWidth / gl.drawingBufferHeight
-  var fieldOfView = Math.PI / 4
-  var near = 0.01
-  var far  = 100
-
-  mat4.perspective(
-      projection
-    , fieldOfView
-    , aspectRatio
-    , near
-    , far
-  )
+//test
+var COLOR = {
+    red: [1.0, 0.0, 0.0]
+  , blue: [0.0, 0.0, 1.0]
+  , green: [0.0, 1.0, 0.0]
+  , black: [1.0, 1.0, 1.0]
 }
+var state = [
+  [COLOR.red, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [COLOR.green, COLOR.green, 0, 0, 0],
+  [COLOR.green, COLOR.green, 0, 0, COLOR.blue]
+]
 
-function render() {
-  update()
-
-  gl.viewport(0, 0, width, height)
-  gl.enable(gl.DEPTH_TEST)
-  gl.enable(gl.CULL_FACE)
-
-  geometry.bind(shader)
-
-  // Update model/view/projection matrices.
-  shader.uniforms.uProjection = projection
-  shader.uniforms.uView = view
-  shader.uniforms.uModel = model
-
-  geometry.draw(gl.TRIANGLES)
-}
+raf(function tick() {
+  render(state)
+  raf(tick)
+})
--- shaders/main.frag
+++ shaders/main.frag
@@ -2,6 +2,8 @@ precision mediump float;
 
 varying vec3 vNormal;
 
+uniform vec3 uColor;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.5, 1.0, 1.0);
+  gl_FragColor = vec4(uColor, 1.0);
 }
